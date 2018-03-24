6a2cb91611a378e77801fe5630daecfb053f008a
Nisha <nisha.nambissan@thetrainline.com>
2018-03-24 13:39:35 +0000

    setup continuous loop for accepting amounts.

--- Clarksons/ATMMachine/ATMMachine/Program.cs
+++ Clarksons/ATMMachine/ATMMachine/Program.cs
@@ -6,7 +6,30 @@ namespace ATMMachine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("*******************************************************\n");
+                Console.WriteLine("Please enter an amount to withdraw (e.g. 120.00 or 5.30");
+                var amount = Console.ReadLine();
+
+                if (Double.TryParse(amount, out double result))
+                {
+                    ProcessInput(result);
+
+                    Console.WriteLine("Press any key to continue X to quit the program");
+                    var input = Console.ReadKey();
+                    if (input.Key == ConsoleKey.X)
+                        break;
+                }
+                else
+                    Console.WriteLine("Could not recognize that as a valid amount.");
+                
+            }
+        }
+
+        private static void ProcessInput(double amountToWithdraw)
+        {
+            Console.WriteLine("\nTest output\n");
         }
     }
 }
